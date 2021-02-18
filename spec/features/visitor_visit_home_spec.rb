e7620064565ccea765a65f1840ccfb0099f80d94
xita Rps <xitarps@gmail.com>
2021-02-18 14:18:56 -0300

    Fix minor issues in tests translations

--- spec/features/visitor_visit_home_spec.rb
+++ spec/features/visitor_visit_home_spec.rb
@@ -9,12 +9,12 @@ feature 'visitor visit homepage' do
   scenario 'successfully(pt-BR)' do
     visit root_path
     click_on 'Português'
-    expect(page).to  have_content('Bem Vindo')
+    expect(page).to  have_content(I18n.t('home.index.title'))
   end
 
   scenario 'successfully(es)' do
     visit root_path
     click_on 'Español'
-    expect(page).to  have_content('Bien Venido')
+    expect(page).to  have_content(I18n.t('home.index.title'))
   end
 end
